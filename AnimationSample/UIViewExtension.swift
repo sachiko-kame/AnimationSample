628db3cc894819df920794de38ab950caa8aeffe
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-03-06 05:39:54 +0900

    デフォルト的なものを作る。大体の基本が入っているはず、諸注意など。

---
+++ AnimationSample/UIViewExtension.swift
@@ -0,0 +1,50 @@
+//
+//  UIViewExtension.swift
+//  AnimationSample
+//
+//  Created by 水野祥子 on 2018/03/06.
+//  Copyright © 2018年 sachiko. All rights reserved.
+//
+
+import UIKit
+
+extension UIView{
+    //基本
+    func boundAnimation(Repeat:Bool = false, ToRepeatTime:Float = 1, EndStop:Bool = true, Speed:Float){
+        let animation = CASpringAnimation(keyPath: "transform.scale")
+        animation.delegate = self
+        animation.fromValue = 0.5 //現在の大きさの何倍から
+        animation.toValue = 3 //現在の大きさの何倍まで
+        animation.speed = Speed //1より少ない遅くなる、　1より多い早くなる　 1がデフォルト　speedは倍率での設定。
+        
+        
+        if(Repeat == true){
+            animation.repeatCount = MAXFLOAT
+            animation.autoreverses = true
+            animation.duration = CFTimeInterval(ToRepeatTime) //アニメーションするまでの時間
+        }
+        
+        
+        if(EndStop == true){ //repeatの状態は意味をなさない
+            //以下3つでアニメーション最後の状態維持可能
+            animation.isRemovedOnCompletion = false //アニメーションを削除するかしないかのflag
+            animation.fillMode = kCAFillModeForwards //アクティブ期間終了時の動作
+            animation.duration = 0 //アニメーションするまでの時間
+        }
+        
+    
+        self.layer.add(animation, forKey: nil)
+    }
+}
+
+extension UIView:CAAnimationDelegate{
+    
+    public func animationDidStart(_ anim: CAAnimation){
+        print("アニメーション開始")
+    }
+    
+    
+    public func animationDidStop(_ anim: CAAnimation, finished flag: Bool){
+        print("アニメーション終了")
+    }
+}
