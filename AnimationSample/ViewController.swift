21f2cd35e9d205cab6f4cccf11664891b43a0e25
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-03-06 04:39:59 +0900

    アニメーションするためのViewを配置

--- AnimationSample/ViewController.swift
+++ AnimationSample/ViewController.swift
@@ -10,14 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sampleView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
